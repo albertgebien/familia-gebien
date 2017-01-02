4aa0ff89b8201ab7d8f07cf1fb291be9d60f820f
Albert.Gebien <albertgebien@gmail.com>
2017-01-02 16:18:48 -0200

    serviços ejbs

--- src/main/java/br/com/familiagebien/servico/impl/PessoaServicoEJB.java
+++ src/main/java/br/com/familiagebien/servico/impl/PessoaServicoEJB.java
@@ -1,17 +1,21 @@
 package br.com.familiagebien.servico.impl;
 
 import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import br.com.familiagebien.entidades.Pessoa;
+import br.com.familiagebien.persistencia.ICrudDAO;
+import br.com.familiagebien.persistencia.dao.PessoaDAO;
 
 @Stateless
-public class PessoaServicoEJB { /*extends BaseServico<Pessoa>  {
+public class PessoaServicoEJB extends BaseServico<Pessoa>  {
 
 	private static final long serialVersionUID = 1L;
 	
 	@Inject
-	private PessoaDao colaboradorDao; 
+	private PessoaDAO pessoaDao; 
 	
     protected ICrudDAO<Pessoa> getDAO() {
-		return this.colaboradorDao;
-	}*/
-
+		return this.pessoaDao;
+	}
 }
