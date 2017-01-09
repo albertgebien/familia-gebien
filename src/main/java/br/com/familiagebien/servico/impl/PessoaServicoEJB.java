e3e30745e54e965b2346636955071b89d250c92e
Albert.Gebien <albertgebien@gmail.com>
2017-01-09 16:40:47 -0200

    registro pessoa

--- src/main/java/br/com/familiagebien/servico/impl/PessoaServicoEJB.java
+++ src/main/java/br/com/familiagebien/servico/impl/PessoaServicoEJB.java
@@ -4,6 +4,8 @@ import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import br.com.familiagebien.entidades.Pessoa;
+import br.com.familiagebien.enums.TipoUsuario;
+import br.com.familiagebien.exceptions.BaseException;
 import br.com.familiagebien.persistencia.ICrudDAO;
 import br.com.familiagebien.persistencia.dao.PessoaDAO;
 
@@ -18,4 +20,28 @@ public class PessoaServicoEJB extends BaseServico<Pessoa>  {
     protected ICrudDAO<Pessoa> getDAO() {
 		return this.pessoaDao;
 	}
+    
+    public Pessoa registrar(Pessoa pessoa) throws BaseException{
+		if("".equals(pessoa.getNome())){
+			throw new BaseException("Nome inválido");
+		}
+		
+		if("".equals(pessoa.getEmail())){
+			throw new BaseException("Email inválido");
+		}
+		
+		if("".equals(pessoa.getSenha())){
+			throw new BaseException("Senha inválido");
+		}
+		
+		Pessoa pessoaPorEmail = this.pessoaDao.obterPorEmail(pessoa.getEmail());
+		if(pessoaPorEmail != null){
+			throw new BaseException("ja existe usuario cadastrado com esse email");
+		}
+		
+		pessoa.setCodTipoUsuario(TipoUsuario.COMUM.getCodigo());
+		pessoa = this.incluir(pessoa);
+    		
+    	return pessoa;
+    }
 }
